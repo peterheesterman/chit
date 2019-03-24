24c9a4aaef870ed73b9dc1428518d04c48f4146b
Peter Heesterman <heesterman.peter@gmail.com>
2019-03-24 17:13:25 +1300

    refactor(Reqwest): DRY up the reqwest use
    * had to introduce a panic to make it simple for now

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate reqwest;
 use colored::*;
 use serde_json::Value;
 use std::env;
-use std::io::Read;
 
 mod format;
 mod crates;
@@ -27,33 +26,8 @@ fn chit(crate_name: String) {
     let width = format::get_width(&crate_name);
     println!("{}", format::title_bar(width, &crate_name));
 
-    let mut res = reqwest::get(&crates::url(&crate_name))
-                    .expect("fail to get crate");
-
-    let mut owners_res = reqwest::get(&crates::owners_url(&crate_name))
-                            .expect("fail to get owners");
-
-    if res.status() != 200 && owners_res.status() != 200 {
-        return println!(
-            "Error: Got back a bad response from crates.io. crate({}) crate_owners({})",
-            res.status(),
-            owners_res.status()
-        );
-    }
-
-    // Crate
-    let mut body = String::new();
-    res.read_to_string(&mut body)
-        .expect("fail to read crate body");
-    let crate_json: Value = serde_json::from_str(&body).expect("fail to serde parse for crate");
-
-    // Crate owners
-    let mut owners_body = String::new();
-    owners_res
-        .read_to_string(&mut owners_body)
-        .expect("fail to read crate owners body");
-    let crate_owners_json: Value =
-        serde_json::from_str(&owners_body).expect("fail to serde parse for crate owners");
+    let crate_json: Value = crates::get(crates::url(&crate_name));
+    let crate_owners_json: Value = crates::get(crates::owners_url(&crate_name));
 
     // Owners
     println!(
